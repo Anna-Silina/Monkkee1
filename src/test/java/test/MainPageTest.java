9858f51f36d92a8ab2ab2736b0a7ec11838ae6e6
Anna <sol11.06@gmail.com>
2022-07-06 23:40:37 +0300

    Merge remote-tracking branch '1/master'

--- src/test/java/test/MainPageTest.java
+++ src/test/java/test/MainPageTest.java
@@ -16,41 +16,42 @@ public class MainPageTest extends BasePageTest {
     private MainPageService mainPageService;
     private SettingPageService settingPageService;
     private AuthorizedPageService authorizedPageService;
+    private AuthorizationPage authorizationPage;
 
     @BeforeTest
     public void setUp() {
         mainPageService = new MainPageService();
         authorizedPageService = new AuthorizedPageService();
         settingPageService = new SettingPageService();
+        authorizationPage = mainPageService.authorization();
     }
 
-    /*@Test(description = "Login")
+    @Test(description = "Login")
     @Description("Fill all fields on main page and login with correct parameters")
     public void loginTest() {
-        AuthorizationPage authorizationPage = mainPageService.authorization();
         String expectedURL = "https://my.monkkee.com/#/entries";
         String actualURL = authorizationPage.clickLogoButton();
         Assert.assertEquals(actualURL, expectedURL, "U didn`t log in");
     }
 
-    @Test(description = "Login")
+   /* @Test(description = "Login")
     @Description("Fill all fields on main page and login with incorrect parameters")
     public void negativeLoginTest() {
         mainPageService.negativeAuthorization();
         String expectedURL = "https://my.monkkee.com/#/";
         String actualURL = driver.getCurrentUrl();
         Assert.assertEquals(actualURL, expectedURL, "U log in with incorrect parameters");
-    }
+    }*/
 
     @Test(description = "Login")
     @Description("Delete test")
     public void deleteTest() {
-        mainPageService.authorization();
+        //mainPageService.authorization();
         settingPageService.clickDeleteLink();
         settingPageService.clickDelete();
         driver.switchTo().alert().accept();
         new WebDriverWait(driver, 15).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'alert-success')]")));
         String text = driver.findElement(By.xpath("//div[contains(@class,'alert-success')]")).getText();
         Assert.assertEquals(text, "You will receive an email with a confirmation link. Please follow the instructions in this email. Please also check your Spam folder!");
-    }*/
+    }
 }
